0bdd340c9ec87de37970fb28e23c0d340f0376f9
ktsaur <taryshkinakv@gmail.com>
2023-12-17 14:25:27 +0300

    добавлен вывод сообщения "я очень люблю джаву"

--- src/main/java/lab1712/Main.java
+++ src/main/java/lab1712/Main.java
@@ -2,6 +2,6 @@ package lab1712;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("я люблю джава");
+        System.out.println("я очень люблю джава");
     }
 }
